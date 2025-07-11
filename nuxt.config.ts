6394bc2ebc2390e01f682f0460a3a94244ad32eb
Patryk Marchut <pm9701133@gmail.com>
2025-07-11 15:50:34 +0200

    Revert "[CHG] Update Nuxt configuration for Netlify deployment"
    This reverts commit 9399c6dfaf4d7d401b9ec7514b8876dc8fc8b6d4.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,6 @@ requireEnvVars();
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: true,
-  nitro: {
-    preset: 'netlify'
-  },
   css: ["@/assets/main.css", "@formkit/themes/genesis"],
   modules: [
     "@formkit/nuxt",
